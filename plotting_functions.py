3a34b2b1c60a7243959b8b6ccc94a1e149f6ec7b
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2023-01-09 16:10:16 +1100

    plot more scalars

--- plotting_functions.py
+++ plotting_functions.py
@@ -13,7 +13,8 @@ def average_best_arm_plot(agents, agent_order, correct_arm, change_freq, save_pa
             lw=4,
             label=label,
         )
-        plt.legend(loc="best")
+
+    plt.legend(loc="best")
 
     plt.xlabel("Steps (per context)")
     plt.ylabel("Average best arm (over context switches)")
@@ -30,7 +31,8 @@ def average_regret_plot(agents, agent_order, regret, change_freq, save_path):
             lw=4,
             label=label,
         )
-        plt.legend(loc="best")
+
+    plt.legend(loc="best")
 
     plt.xlabel("Steps (per context)")
     plt.ylabel("Average regret (over context switches)")
@@ -44,14 +46,109 @@ def cumulative_regret(agents, agent_order, regret, save_path):
 
     for i, name in enumerate(agent_order):
         label = agents[name][0]
-        means = regret[i].mean(0).ravel().cumsum(-1)
+        flattened_regrets = np.reshape(regret[i], (regret[i].shape[0], -1))
+        cum_regret = np.cumsum(flattened_regrets, axis=1)
+        means = cum_regret.mean(0)
+        stds = cum_regret.std(0)
+        # means = regret[i].mean(0).ravel().cumsum(-1)
         # stds = regret[i].std(0).ravel()
-        h = plt.plot(means, lw=2, label=label)
-        # plt.fill_between(range(len(stds)), means - stds, means + stds, alpha=0.5)
+        plt.plot(means, lw=2, label=label)
+        plt.fill_between(range(len(stds)), means - stds, means + stds, alpha=0.25)
 
-        plt.legend(loc="best")
+    plt.legend(loc="best")
 
     plt.xlabel("Steps")
     plt.ylabel("Cumulative regret")
 
     fig.savefig(os.path.join(save_path, "cumulative_regret.pdf"))
+
+
+def learning_rates(agents, agent_order, learning_rates, save_path):
+    fig = plt.figure()
+    for i, name in enumerate(agent_order):
+        plt.plot(learning_rates[i].mean(0).ravel(), label=agents[name][0])
+    plt.yscale("log")
+    plt.xlabel("Steps")
+    plt.ylabel("Learning Rate")
+    plt.legend(loc="best")
+    fig.savefig(os.path.join(save_path, "learning_rates.pdf"))
+
+
+def temperatures(agents, agent_order, temperatures, save_path):
+    fig = plt.figure()
+    for i, name in enumerate(agent_order):
+        plt.plot(temperatures[i].mean(0).ravel(), label=agents[name][0])
+    plt.yscale("log")
+    plt.xlabel("Steps")
+    plt.ylabel("Temperature")
+    plt.legend(loc="best")
+    fig.savefig(os.path.join(save_path, "temperatures.pdf"))
+
+
+def min_uncertainties(agents, agent_order, min_uncertainties, save_path):
+    fig = plt.figure()
+    for i, name in enumerate(agent_order):
+        plt.plot(min_uncertainties[i].mean(0).ravel(), label=agents[name][0])
+    plt.yscale("log")
+    plt.xlabel("Steps")
+    plt.ylabel("Minimum Epistemic Uncertainty")
+    plt.legend(loc="best")
+    fig.savefig(os.path.join(save_path, "min_uncertainties.pdf"))
+
+
+def policies(agents, agent_order, policies, save_path):
+    fig = plt.figure()
+    for i, name in enumerate(agent_order):
+        plt.plot(policies[i].max(-1).mean(0).ravel(), label=agents[name][0])
+    plt.xlabel("Steps")
+    plt.ylabel("Policy")
+    plt.legend(loc="best")
+    fig.savefig(os.path.join(save_path, "policies.pdf"))
+
+
+def mean_mses(agents, agent_order, mses, save_path):
+    fig = plt.figure()
+    for i, name in enumerate(agent_order):
+        plt.plot(mses[i, ..., 0].mean(0).ravel(), label=agents[name][0])
+    plt.xlabel("Steps")
+    plt.ylabel("Mean MSEs")
+    plt.legend(loc="best")
+    fig.savefig(os.path.join(save_path, "mean_mses.pdf"))
+
+
+def variance_mses(agents, agent_order, mses, save_path):
+    fig = plt.figure()
+    for i, name in enumerate(agent_order):
+        plt.plot(mses[i, ..., 1].mean(0).ravel(), label=agents[name][0])
+    plt.xlabel("Steps")
+    plt.ylabel("Variance MSEs")
+    plt.legend(loc="best")
+    fig.savefig(os.path.join(save_path, "variance_mses.pdf"))
+
+
+def scalar_plot(agents, agent_order, scalar_data, save_path, label, logscale):
+    fig = plt.figure()
+    for i, name in enumerate(agent_order):
+        plt.plot(scalar_data[i].mean(0).ravel(), label=agents[name][0])
+    if logscale:
+        plt.yscale("log")
+    plt.xlabel("Steps")
+    plt.ylabel(label)
+    plt.legend(loc="best")
+    fig.savefig(save_path)
+
+
+# def distributions(distributions, change_frequency, save_path):
+#     fig, axes = plt.subplots(1, 2)
+
+#     import pdb
+
+#     pdb.set_trace()
+
+#     axes[0].plot(np.repeat(distributions, change_frequency, axis=0)[:, 0], lw=4)
+#     axes[0].set_title("Mean")
+
+#     axes[1].plot(np.repeat(distributions, change_frequency, axis=0)[:, 1], lw=4)
+#     axes[1].set_title("Standard Deviation")
+
+#     fig.savefig(os.path.join(save_path, "distributions.pdf"))

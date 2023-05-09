38875c4f0925f70ce57a36dde72106ed19f974fb
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2023-05-09 12:57:12 +0100

    filled in

--- dd_bandits/experiments/config_changes.py
+++ dd_bandits/experiments/config_changes.py
@@ -4,14 +4,35 @@ import numpy as n
 
 NUM_ARMS = 5
 
+constant_agents = []
+for lr in []:
+    for t in []:
+        for ud in [True, False]:
+            constant_agents.append(
+                {
+                    "name": f"constant_{t}_{lr}",
+                    "agent": "td_doya_dayu",
+                    "n_ens": 10,
+                    "adaptation_modules": {"constant": lr, "constant_t": t},
+                    "learning_rate": "constant",
+                    "temperature": "constant_t",
+                    "mask_p": 1.0,
+                    "q_initialisation": 0.01,
+                    "s_initialisation": 0.01,
+                    "lr_per_arm": True,
+                    "scalar_log_spec": ["mean_mean", "mean_var", "var_mean", "var_var"],
+                    "use_direct": ud,
+                }
+            )
+
 # doya-dayu agents
 dd_agents = []
-for ens in []:
-    for m in []:
-        for lr in []:
-            for q_init in []:
-                for s_init in []:
-                    for mask in []:
+for ens in [5, 10, 20]:
+    for m in [0.1, 0.5, 1, 2]:
+        for lr in [0.1, 0.25, 1]:
+            for q_init in [0.01, 0.0, 0.1]:
+                for s_init in [0.01, 0.0, 0.1, 1.0]:
+                    for mask in [1.0, 0.25, 0.5, 0.75]:
                         for ud in [True, False]:
                             dd_agents.append(
                                 {
@@ -58,9 +79,9 @@ for ens in []:
 
 # boltzmann agents
 boltzmann_agents = []
-for t in []:
-    for lr in []:
-        for q_init in []:
+for t in [0.01, 0.05, 0.1, 0.25]:
+    for lr in [0.1, 0.25, 0.5, 1]:
+        for q_init in [0.01]:
             boltzmann_agents.append(
                 {
                     "name": f"boltzmann_{t}_{lr}_{q_init}",
@@ -74,10 +95,10 @@ for t in []:
 
 # ducb agents
 ducb_agents = []
-for r in []:
-    for g in []:
-        for lr in []:
-            for q_init in []:
+for r in [1.0]:
+    for g in [0.9, 0.99, 0.999, 0.9999]:
+        for lr in [0.01, 0.05, 0.2]:
+            for q_init in [0.01]:
                 ducb_agents.append(
                     {
                         "name": f"ducb_{r}_{g}_{lr}_{q_init}",
@@ -94,3 +115,7 @@ for r in []:
                         ],
                     }
                 )
+
+agents = constant_agents + dd_agents + boltzmann_agents + ducb_agents
+
+CONFIG_CHANGES = {"agent_ablation": [{"agents": [agents]}]}
